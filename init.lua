696a9fada0ca08a8ca516e8674e3a607ad8c0448
yongruifang <yongruifang@163.com>
2024-01-21 19:25:23 +0800

    feat(add lazy): install lazy to be our package manager

--- init.lua
+++ init.lua
@@ -3,3 +3,20 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+
+-- install package manager -- Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+plugins={}
+opts={}
+require("lazy").setup(plugins, opts)

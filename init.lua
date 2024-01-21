98307c5f41da0d2fbfd0904fb9d657d27127cbfa
yongruifang <yongruifang@163.com>
2024-01-21 19:52:37 +0800

    feat(live_grep): a command to find matched content, rather than file_names
    configure vim.g.mapleaded, useful!

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.g.mapleader = " "
 
 -- install package manager -- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -37,3 +38,5 @@ require("telescope").setup{
     find_files={theme="cursor"}
   }
 }
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+

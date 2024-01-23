384b15bc58d4cbe4af41afc151b0e2f4da31f50e
yongruifang <yongruifang@163.com>
2024-01-23 09:59:33 +0800

    feat(eslint_d and prettier): declaure null_ls.builtins.xxx, use Mason to install xxx

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,8 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.diagnostics.eslint_d,
         },
       })
       vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})

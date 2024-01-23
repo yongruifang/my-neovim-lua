57b8607def323fc0a98c8e0c00592fd216d699ed
yongruifang <yongruifang@163.com>
2024-01-23 10:36:32 +0800

    feat(completion and snippet): nvim_cmp, luasnip, cmp-nvim-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,8 +16,11 @@ return {
   , {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig=require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilites
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({"n"}, "<leader>ca", vim.lsp.buf.code_action, {})

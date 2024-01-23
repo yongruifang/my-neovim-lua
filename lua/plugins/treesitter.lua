7718401695854bf01c6cfed676e3932f016b4e0e
yongruifang <yongruifang@163.com>
2024-01-23 09:52:05 +0800

    feat(treesitter): auto_install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,8 @@ return   {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed={"lua","javascript"},
+      -- ensure_installed={"lua","javascript"},
+      auto_install = true,
       highlight={enable=true},
       indent={enable=true},
     })

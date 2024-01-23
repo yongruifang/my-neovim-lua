6f3b62db04a0f8627b3bb1c0ebcee841e3ce88b9
yongruifang <yongruifang@163.com>
2024-01-23 09:29:59 +0800

    feat(lsp): mason, mason-lspconfig; nvim-lspconfig(+ key-map), telescope-ui-select
    Mason can manage LSP,DAP,linters,formatters, add keymap K(hover),gd(GotoDef), ca(CodeAction)

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,7 +5,7 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim"
-  },
+  },  
   config = function ()
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
   end

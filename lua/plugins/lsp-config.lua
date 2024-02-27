a3449d799810612c7129f9de1ede8111d7579755
Fang Yongrui <1102649231@qq.com>
2024-02-27 17:16:14 +0800

    fix(spell error): capabilities
    in lsp-config, mason.nvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,7 +2,9 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup()
+      require("mason").setup({
+        PATH = "prepend", -- "skip" seems to cause the spawing error
+      })
     end
   }
   , {
@@ -20,7 +22,7 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig=require("lspconfig")
       lspconfig.lua_ls.setup({
-        capabilities = capabilites
+        capabilities = capabilities
       })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,8 @@ return   {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed={"lua","javascript", "scala","markdown","markdown_inline"},
-      -- auto_install = true,
+      ensure_installed = {},
+      auto_install = true,
       highlight={enable=true},
       indent={enable=true},
     })

42dbd05ddd150e4e5ff35e9f45ff8008c5660cfb
Fang Yongrui <1102649231@qq.com>
2024-03-16 11:26:45 +0800

    keymap(term): quit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
+
